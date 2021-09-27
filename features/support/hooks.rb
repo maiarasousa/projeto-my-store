2b4932d4167ae6128d3d81aa4ff2516303e8f00d
maiara sousa <maivelososousa@gmail.com>
2021-09-27 18:47:53 -0300

    first commit

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,21 +1,21 @@
 Before do
-    @pagina_inicial = PaginaInicial.new
-    @submenu = Submenu.new
-    @formulario = Formulario.new
-    @carrinho = Carrinho.new
-    @login = Login.new
-    @pesquisar = Pesquisar.new
-    @novo_endereco = NovoEndereco.new
+  @pagina_inicial = PaginaInicial.new
+  @submenu = Submenu.new
+  @formulario = Formulario.new
+  @carrinho = Carrinho.new
+  @login = Login.new
+  @pesquisar = Pesquisar.new
+  @novo_endereco = NovoEndereco.new
 end
 
 After do |scenario| 
-    name = scenario.name
-    temp_shot = page.save_screenshot("allure-results/evidences/#{name}/temp_screenshot.png")
+  name = scenario.name
+  temp_shot = page.save_screenshot("allure-results/evidences/#{name}/temp_screenshot.png")
     
-    Allure.add_attachment(
-      name: name,
-      type: Allure::ContentType::PNG, 
-      source: File.open(temp_shot)
+  Allure.add_attachment(
+    name: name,
+    type: Allure::ContentType::PNG, 
+    source: File.open(temp_shot)
     )
   end
   
